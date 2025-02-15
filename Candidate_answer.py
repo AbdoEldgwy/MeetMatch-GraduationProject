873191b85d1e9ea992edc6a8dd58cc96a27a297a
AbdoEldgwy <abdo.eldgwy1@gmail.com>
2025-02-15 17:57:29 +0200

    add stt and update some code

--- Candidate_answer.py
+++ Candidate_answer.py
@@ -1,3 +1,11 @@
-# This is the candidate answer from Speech to text Api.
+from datetime import datetime
 class CandidateAnswer:
-    pass
+    def __init__(self,question_id:int ,answer_text:str, timestamp: datetime):
+        self.question_id = question_id
+        self.answer_text = answer_text
+        self.timestamp = timestamp
+        
+        
+    def get_answer(self):
+        # code to get the answer for the given question using the Speech to Text Api
+        pass
--- Interview_Session.py
+++ Interview_Session.py
@@ -1,4 +1,5 @@
 from .Candidate_answer import CandidateAnswer
-
+from datetime import datetime
 class InterviewSession:
-    pass
+    def __init__(self, sessino_id:int, timestamp: datetime):
+        self.sessino_id = sessino_id
