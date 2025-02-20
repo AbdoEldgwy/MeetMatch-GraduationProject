c19803440ded172fbb1a65e8e539d76b4a4833ce
AbdoEldgwy <abdo.eldgwy1@gmail.com>
2025-02-20 03:09:30 +0200

    init Question Manager related class

--- src/Question_Manager.py
+++ src/Question_Manager.py
@@ -1,3 +1,4 @@
+from typing import List, Optional
 class JSONFileHandler:
     def __init__(self, file_path: str):
         self.file_path = file_path
@@ -33,3 +34,30 @@ class Question:
 
     def get_related_kpis(self) -> list[str]:
         return self.related_kpis
+
+
+class QuestionManager:
+
+    def __init__(self):
+        self.questions: List[Question] = []
+        self._current_index = 0
+        
+    def load_questions_from_file(self, file_handler: JSONFileHandler):
+        """Load and parse questions using a FileHandler"""
+        raw_data = file_handler.load_questions()
+        self.questions = file_handler.parse_questions(raw_data)
+        self._current_index = 0  # Reset pointer on new load
+        
+    def get_next_question(self) -> Optional[Question]:
+        """Get next question in sequence"""
+        if self._current_index < len(self.questions):
+            question = self.questions[self._current_index]
+            self._current_index += 1
+            return question
+        return None
+    
+    def get_question_by_id(self, q_id: str) -> Optional[Question]:
+        for i in range(len(self.questions)):
+            if self.questions[i].id == q_id:
+                return self.questions[i]
+        return None
