c6882a73e6968732c56a596184dadc0abdafa7f0
AbdoEldgwy <abdo.eldgwy1@gmail.com>
2025-02-20 02:19:01 +0200

    push GPT_API class

--- src/GPT_API.py
+++ src/GPT_API.py
@@ -1,3 +1,24 @@
-class GPTAPIClient:
+from google import genai 
+
+class GeminiClient:
     def __init__(self):
-        pass
+        _api_key = "api_key"
+        self.model = "gemini-2.0-flash"
+        self.client = genai.Client(api_key=_api_key)
+
+    def get_response(self, prompt: str, dump_json: bool = False) -> str:
+        
+        try:
+            response = self.client.models.generate_content(
+                model=self.model,
+                contents=prompt
+                )
+            if not response.text:
+                raise ValueError("Gemini API returned empty response")
+            if dump_json:
+                return response.text,response.model_dump_json(
+                        exclude_none=True, indent=4)
+            return response.text
+                
+        except Exception as e:
+            raise AttributeError(f"Gemini API Error: {str(e)}")
